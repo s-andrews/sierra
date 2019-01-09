86168d490a742b1c286acf338e1b5ae7682216fe
FelixKrueger <felix.krueger@babraham.ac.uk>
2019-01-09 16:21:59 +0000

    Added mySQL commands necessary for library prep

---
+++ SQL/add_library_prep.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE library_prep (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(200),
+       allows_subsamples BOOLEAN,
+       require_prep BOOLEAN	
+);
+
+INSERT INTO library_prep (name,allows_subsamples,require_prep) VALUES ("Pre-prepared library",1,0);
+
+ALTER TABLE sample ADD library_prep_id INT NOT NULL DEFAULT 1;
+
+ALTER TABLE sample ADD KEY library_prep_id (library_prep_id);
+
+
+ALTER TABLE barcode ADD passed_qc BOOLEAN NOT NULL DEFAULT 1;
+
+ALTER TABLE barcode ADD KEY passed_qc (passed_qc);
+
+
