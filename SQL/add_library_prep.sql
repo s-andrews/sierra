2ecea3400a7309c66454899c8e8b02ba6b26a6b2
FelixKrueger <felix.krueger@babraham.ac.uk>
2019-01-28 16:55:13 +0000

    Added retired flag to library_prep

--- SQL/add_library_prep.sql
+++ SQL/add_library_prep.sql
@@ -3,7 +3,8 @@ CREATE TABLE library_prep (
        id INT AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(200),
        allows_subsamples BOOLEAN,
-       require_prep BOOLEAN	
+       require_prep BOOLEAN,
+       retired BOOLEAN NOT NULL DEFAULT 0       	
 );
 
 INSERT INTO library_prep (name,allows_subsamples,require_prep) VALUES ("Pre-prepared library",1,0);
@@ -16,5 +17,6 @@ ALTER TABLE sample ADD KEY library_prep_id (library_prep_id);
 ALTER TABLE barcode ADD passed_qc BOOLEAN NOT NULL DEFAULT 1;
 
 ALTER TABLE barcode ADD KEY passed_qc (passed_qc);
+
 
 
